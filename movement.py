f971fa7ebc0dafa966fc50639ae983965f242d83
Fer1212 <65574140+Fer1212@users.noreply.github.com>
2020-08-16 10:27:25 -0300

    Add files via upload

---
+++ movement.py
@@ -0,0 +1,34 @@
+import time
+import serial
+import smtplib
+
+TO = 'fernandasophia2017@gmail.com' #Email that the message will be sent to
+GMAIL_USER = 'sgfaapp@gmail.com' #Email that will send the message
+GMAIL_PASS = 'sgfaapp2020' #Password of the email that the message is sent from
+
+SUBJECT = 'Are you okay?'
+TEXT = 'Your PIR sensor detected movement' 
+  
+ser = serial.Serial('COM4', 9600)
+
+def send_email():
+    print("Sending Email")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + SUBJECT + '\n'
+    print header
+    msg = header + '\n' + TEXT + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, TO, msg)
+    smtpserver.close()
+    
+while True:
+    message = ser.readline()
+    print(message)
+    if message[0] == 'M' :
+        send_email()
+    time.sleep(0.5)
+
